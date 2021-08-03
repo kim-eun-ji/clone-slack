40f73e9b7a588bb966fbb7b7360672ab5f4f05f5
kim-eun-ji <qwert101311@gmail.com>
2021-08-03 20:32:06 +0900

    Update features

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
+
     @Get()
-    getUsers() {}
+    getUsers(@Req() req) {
+        return req.user;
+    }
 
     @Post()
-    postUsers() { }
+    postUsers(@Body() data: JoinRequestDto) { 
+        this.usersService.postUsers(data.email, data.nickname, data.password);
+    }
 
     @Post('login')
     logIn(@Req() req) {
