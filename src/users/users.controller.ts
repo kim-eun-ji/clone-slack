74e8d385e28cc16342d0299d04e859ce9a8c182b
kim-eun-ji <qwert101311@gmail.com>
2021-08-06 22:28:43 +0900

    create custom decorator - user, token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
 import { UserDto } from 'src/common/dto/user.dto';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
@@ -19,8 +20,8 @@ export class UsersController {
     })
     @ApiOperation({summary: '내 정보 조회'})
     @Get()
-    getUsers(@Req() req) {
-        return req.user;
+    getUsers(@User() user) {
+        return user;
     }
 
     @ApiOperation({summary: '회원가입'})
@@ -35,8 +36,8 @@ export class UsersController {
     })
     @ApiOperation({summary: '로그인'})
     @Post('login')
-    logIn(@Req() req) {
-        return req.user;
+    logIn(@User() user) {
+        return user;
      }
 
     @ApiOperation({summary: '로그아웃'})
