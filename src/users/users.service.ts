66cd9426d955db96f867e9b813c72af334fab3ba
kim-eun-ji <qwert101311@gmail.com>
2021-08-16 19:06:03 +0900

    Remove ignored file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
+    getUser() { }
     postUsers(email: string, nickname: string, password: string) {
         throw new Error('Method not implemented.');
     }
