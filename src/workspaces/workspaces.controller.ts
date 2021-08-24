17e38bd2017eb6ca96360984d59acdea6fa3b3a9
kim-eun-ji <qwert101311@gmail.com>
2021-08-24 17:25:03 +0900

    Feat get, create workspace func

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Delete, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+import { User } from "src/common/decorator/user.decorator";
+import { Users } from "src/entities/Users";
+import { CreateWorkspaceDto } from "./dto/create-workspace.dto";
+import { WorkspacesService } from "./workspaces.service";
 
 @ApiTags("WORKSPACE")
 @Controller("api/workspaces")
 export class WorkspacesController {
+  constructor(private WorkspacesService: WorkspacesService) {}
+
   @Get()
-  getMyWorkspaces() {}
+  getMyWorkspaces(@User() user: Users) {
+    return this.WorkspacesService.findMyWorkspaces(user.id);
+  }
 
   @Post()
-  createWorkspace() {}
+  createWorkspace(@User() user: Users, @Body() body: CreateWorkspaceDto) {
+    return this.WorkspacesService.createWorkspace(body.workspace, body.url, user.id);
+  }
 
   @Get(":url/members")
   getAllMembersFromWorkspace() {}
