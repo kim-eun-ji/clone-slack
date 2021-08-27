1c38c7ba76ff464f329cb9ed34e6b87f224c15f8
kim-eun-ji <qwert101311@gmail.com>
2021-08-27 17:51:34 +0900

    Feat channel api

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ChannelChats } from "src/entities/ChannelChats";
+import { ChannelMembers } from "src/entities/ChannelMembers";
+import { Channels } from "src/entities/Channels";
+import { Users } from "src/entities/Users";
 import { ChannelsController } from "./channels.controller";
 import { ChannelsService } from "./channels.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Channels, ChannelMembers, ChannelChats, Users])],
   controllers: [ChannelsController],
   providers: [ChannelsService]
 })
