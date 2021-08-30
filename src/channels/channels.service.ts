9f8841f4a87ba82faffc6a76a275fb63a392f249
kim-eun-ji <qwert101311@gmail.com>
2021-08-30 22:11:26 +0900

    Feat channel apis

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ChannelChats } from "src/entities/ChannelChats";
 import { ChannelMembers } from "src/entities/ChannelMembers";
 import { Channels } from "src/entities/Channels";
 import { Users } from "src/entities/Users";
 import { Workspaces } from "src/entities/Workspaces";
-import { Repository } from "typeorm";
+import { MoreThan, Repository } from "typeorm";
 
 @Injectable()
 export class ChannelsService {
@@ -51,15 +51,110 @@ export class ChannelsService {
     });
   }
 
-  async getWorkspaceChannelMembers() {}
+  async createWorkspaceChannels(url: string, name: string, myId: number) {
+    const workspace = await this.workspacesRepository.findOne({
+      where: { url }
+    });
 
-  async createWorkspaceChannelMembers() {}
+    const channel = new Channels();
+    channel.name = name;
+    channel.WorkspaceId = workspace.id;
 
-  async getWorkspaceChannelChats() {}
+    const channelReturned = await this.channelsRepository.save(channel);
+    const channelMember = new ChannelMembers();
+    channelMember.UserId = myId;
+    channelMember.ChannelId = channelReturned.id;
+
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceChannelMembers(url: string, name: string) {
+    return this.usersRepository
+      .createQueryBuilder("user")
+      .innerJoin("user.Channels", "channels", "channels.name = :name", { name })
+      .innerJoin("channels.Workspace", "workspace", "workspace.url = :url", {
+        url
+      })
+      .getMany();
+  }
+
+  async createWorkspaceChannelMembers(
+    url: string,
+    name: string,
+    email: string
+  ) {
+    // 1. 채널 찾기
+    const channel = await this.channelsRepository
+      .createQueryBuilder("channel")
+      .innerJoin("channel.Workspace", "workspace", "workspace.url = :url", {
+        url
+      })
+      .where("channel.name = :name", { name })
+      .getOne();
+    if (!channel) {
+      throw new NotFoundException("채널이 존재하지 않습니다.");
+    }
+
+    // 2. 사용자 찾기
+    const user = await this.usersRepository
+      .createQueryBuilder("user")
+      .where("user.email = :email", { email })
+      .innerJoin("user.Workspaces", "workspace", "workspace.url = :url", {
+        url
+      })
+      .getOne();
+    if (!user) {
+      throw new NotFoundException("사용자가 존재하지 않습니다.");
+    }
+
+    // 3. 채널 멤버 추가
+    const channelMember = new ChannelMembers();
+    channelMember.ChannelId = channel.id;
+    channelMember.UserId = user.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  // 채팅 내역 가져오기
+  async getWorkspaceChannelChats(
+    url: string,
+    name: string,
+    perPage: number,
+    page: number
+  ) {
+    return this.channelChatsRepository
+      .createQueryBuilder("channelChats")
+      .innerJoin("channelChats.Channel", "channel", "channel.name = :name", {
+        name
+      })
+      .innerJoin("channel.Workspace", "workspace", "workspace.url = :url", {
+        url
+      })
+      .innerJoinAndSelect("channelChats.User", "user")
+      .orderBy("channelChats.createAt", "DESC")
+      .take(perPage)
+      .skip(perPage * (page - 1))
+      .getMany();
+  }
+
+  // 읽지 않은 메시지 개수
+  async getChannelUnreadsCount(url: string, name: string, after: string) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder("channel")
+      .innerJoin("channel.Workspace", "workspace", "workspace.url = :url", {
+        url
+      })
+      .where("channel.name = :name", { name })
+      .getOne();
+
+    return this.channelChatsRepository.count({
+      where: {
+        ChannelId: channel.id,
+        createAt: MoreThan(new Date(after))
+      }
+    });
+  }
 
   async createWorkspaceChannelChats() {}
 
   async createWorkspaceChannelImages() {}
-
-  async getChannelUnreadsCount() {}
 }
