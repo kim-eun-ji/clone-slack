3009556cdda1963dad1cd3e9023587d9f4a82498
kim-eun-ji <qwert101311@gmail.com>
2021-09-07 19:43:38 +0900

    <feature> : static 세팅 - CORS 설정 - useStaticAssets 설정

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,35 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./http-exception.filter";
 import passport from "passport";
+import path from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
-  app.useGlobalPipes(new ValidationPipe()); // transform: true 설정 시 param 타입 변환 자동 => pipe 사용x
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe()); // transform: true 설정 시 param 타입 변환 자동 => pipe 사용x
+
+  // static setting
+  if (process.env.NODE_ENV === "production") {
+    app.enableCors({
+      origin: ["https://실주소"],
+      credentials: true
+    });
+  } else {
+    app.enableCors({
+      origin: true,
+      credentials: true
+    });
+  }
+  // useStaticAssets 못찾는 이유
+  // nest가 express 혹은 fastify를 내부에서 선택하는데 선택한 것에 해당 함수가 없는것.
+  // app생성시 익스프레스로 사용하기위해 제네릭 타입으로 NestExpressApplication를 해주어 타입추론이 가능하게 한다.
+  app.useStaticAssets(path.join(__dirname, "..", "uploads"), {
+    prefix: "/uploads"
+  });
 
   const config = new DocumentBuilder()
     .setTitle("Slack example")
