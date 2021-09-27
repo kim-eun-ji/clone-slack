6bdbc340d0afdf0d71439164693cc51921d54f01
kim-eun-ji <qwert101311@gmail.com>
2021-09-27 21:27:17 +0900

    <fix> : eventgateway 생성 오류 수정 provider에 eventsGateway를 넣어 매번 서버를 새로 생성하는 오류 수정

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { EventsGateway } from "./events.gateway";
 
-@Module({})
+@Module({ exports: [EventsGateway] })
 export class EventsModule {
   providers: [EventsGateway];
 }
