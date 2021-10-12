a08df7e00301865627f8c2a05775dcb061aa9851
kim-eun-ji <qwert101311@gmail.com>
2021-10-12 20:59:20 +0900

    <test> : fix error - promise, mock data

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,7 +3,22 @@ import { getRepositoryToken } from "@nestjs/typeorm";
 import { Users } from "src/entities/Users";
 import { UsersService } from "./users.service";
 
-class MockUserRepository {}
+class MockUserRepository {
+  // 가짜 private 데이터 및 가짜 함수
+  #data = [
+    {
+      email: "qwert@na.cpom",
+      id: 1
+    }
+  ];
+  findOne({ where: { email } }) {
+    const data = this.#data.find(v => v.email === email);
+    if (data) {
+      return data;
+    }
+    return null;
+  }
+}
 class MockworkspaveMembersRepository {}
 class MockchannelMembersRepository {}
 
@@ -37,13 +52,13 @@ describe("UsersService", () => {
   });
 
   it("findByEmail은 이메일을 통해 유저를 조회함", () => {
-    expect(service.findByEmail("qwert@na.cpom")).toBe({
+    expect(service.findByEmail("qwert@na.cpom")).resolves.toStrictEqual({
       email: "qwert@na.cpom",
       id: 1
     });
   });
 
   it("findByEmail은 유저를 찾지 못하면 null을 반환해야 함", () => {
-    expect(service.findByEmail("qwert@naver.cpom")).toBe(null);
+    expect(service.findByEmail("qwert@naver.cpom")).resolves.toBe(null);
   });
 });
